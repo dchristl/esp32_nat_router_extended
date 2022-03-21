057fb23e53419aabc42ca85f7b66024ce6ab8cc2
danny <smsoip@gmail.com>
2022-03-21 19:41:53 +0100

    more cleanups and simplification closes #25

--- src/urihandler/statichandler.c
+++ src/urihandler/statichandler.c
@@ -41,10 +41,13 @@ esp_err_t jquery_get_handler(httpd_req_t *req)
 // Handler to download a "favicon.ico" file kept on the server
 esp_err_t favicon_get_handler(httpd_req_t *req)
 {
-    extern const unsigned char favicon_ico_start[] asm("_binary_favicon_ico_start");
+    extern const char favicon_ico_start[] asm("_binary_favicon_ico_start");
+    extern const char favicon_ico_end[] asm("_binary_favicon_ico_end");
+    const size_t favicon_ico_size = (favicon_ico_end - favicon_ico_start);
     httpd_resp_set_type(req, "image/x-icon");
     ESP_LOGI(TAG_HANDLER, "Requesting favicon");
-    return download(req, (const char *)favicon_ico_start);
+    closeHeader(req);
+    return httpd_resp_send(req, favicon_ico_start, favicon_ico_size);
 }
 
 esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
@@ -57,11 +60,9 @@ esp_err_t reset_get_handler(httpd_req_t *req)
 {
     httpd_req_to_sockfd(req);
     extern const char reset_start[] asm("_binary_reset_html_start");
-    extern const char reset_end[] asm("_binary_reset_html_end");
-    const size_t reset_html_size = (reset_end - reset_start);
 
     closeHeader(req);
 
-    esp_err_t ret = httpd_resp_send(req, reset_start, reset_html_size);
+    esp_err_t ret = httpd_resp_send(req, reset_start, HTTPD_RESP_USE_STRLEN);
     return ret;
 }
