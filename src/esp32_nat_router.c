7881aa9c85a97d157ad4331c7b619aa7e93152cb
danny <smsoip@gmail.com>
2023-07-07 17:16:15 +0200

    Fixed type

--- src/esp32_nat_router.c
+++ src/esp32_nat_router.c
@@ -403,10 +403,10 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
-        ESP_LOGI(TAG, "disconnected - retry to connect to the AP");
+        ESP_LOGI(TAG, "disconnected - retry to connect to the STA");
         ap_connect = false;
         esp_wifi_connect();
-        ESP_LOGI(TAG, "retry to connect to the AP");
+        ESP_LOGI(TAG, "retry to connect to the STA");
         xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
     }
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
